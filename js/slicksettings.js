2313d03fa27d7074d2cb17954344cd8869eb38bf
Kamil <kamilrychu@gmail.com>
2022-10-25 00:36:21 +0200

    website from 2 part of MMCS

---
+++ js/slicksettings.js
@@ -0,0 +1,21 @@
+$('.team-carousel').slick({
+	arrows: false,
+	autoplay: true,
+	mobilefirst: true,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	responsive: [
+		{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 1,
+			},
+		},
+		{
+			breakpoint: 992,
+			settings: {
+				slidesToShow: 2,
+			},
+		},
+	],
+})
